02faa9a9ac8aa4daa66fa6f687f6fd17d0329b46
Maxim Kulkin <maxim.kulkin@gmail.com>
2017-12-05 10:00:06 -0800

    Initial import

---
+++ component.mk
@@ -0,0 +1,13 @@
+
+wolfssl_VERSION = 3.12.2
+wolfssl_THIRDPARTY_ROOT = $(wolfssl_ROOT)wolfssl-$(wolfssl_VERSION)
+
+INC_DIRS += $(wolfssl_ROOT) $(wolfssl_THIRDPARTY_ROOT)
+
+wolfssl_INC_DIR = $(wolfssl_THIRDPARTY_ROOT)
+wolfssl_SRC_DIR = $(wolfssl_THIRDPARTY_ROOT)/src $(wolfssl_THIRDPARTY_ROOT)/wolfcrypt/src
+
+EXTRA_CFLAGS += -DWOLFSSL_USER_SETTINGS
+
+$(eval $(call component_compile_rules,wolfssl))
+
