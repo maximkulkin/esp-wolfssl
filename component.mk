62252d705acc7156b1217a22e9bb78e3de26936a
Maxim Kulkin <maxim.kulkin@gmail.com>
2018-02-12 02:14:01 -0800

    Add support for ESP_IDF SDK

--- component.mk
+++ component.mk
@@ -1,13 +1,22 @@
-
 wolfssl_VERSION = 3.13.0-stable
-wolfssl_THIRDPARTY_ROOT = $(wolfssl_ROOT)wolfssl-$(wolfssl_VERSION)
 
-INC_DIRS += $(wolfssl_ROOT) $(wolfssl_THIRDPARTY_ROOT)
+ifdef component_compile_rules
+    # ESP_OPEN_RTOS
+    wolfssl_THIRDPARTY_ROOT = $(wolfssl_ROOT)wolfssl-$(wolfssl_VERSION)
 
-wolfssl_INC_DIR = $(wolfssl_THIRDPARTY_ROOT)
-wolfssl_SRC_DIR = $(wolfssl_THIRDPARTY_ROOT)/src $(wolfssl_THIRDPARTY_ROOT)/wolfcrypt/src
+    INC_DIRS += $(wolfssl_ROOT) $(wolfssl_THIRDPARTY_ROOT)
 
-EXTRA_CFLAGS += -DWOLFSSL_USER_SETTINGS
+    wolfssl_INC_DIR = $(wolfssl_THIRDPARTY_ROOT)
+    wolfssl_SRC_DIR = $(wolfssl_THIRDPARTY_ROOT)/src $(wolfssl_THIRDPARTY_ROOT)/wolfcrypt/src
 
-$(eval $(call component_compile_rules,wolfssl))
+    EXTRA_CFLAGS += -DESP_OPEN_RTOS -DWOLFSSL_USER_SETTINGS
+
+    $(eval $(call component_compile_rules,wolfssl))
+else
+    # ESP_IDF
+    wolfssl_THIRDPARTY_ROOT = wolfssl-$(wolfssl_VERSION)
+
+    COMPONENT_SRCDIRS = $(wolfssl_THIRDPARTY_ROOT)/src $(wolfssl_THIRDPARTY_ROOT)/wolfcrypt/src
+    COMPONENT_ADD_INCLUDEDIRS = . include $(wolfssl_THIRDPARTY_ROOT)
+endif
 
--- user_settings.h
+++ user_settings.h
@@ -1,12 +1,40 @@
 #ifndef wolfcrypt_user_settings_h
 #define wolfcrypt_user_settings_h
 
+#ifdef ESP_IDF
+
+#include <esp_system.h>
+
+static inline int hwrand_generate_block(uint8_t *buf, size_t len) {
+    int i;
+    for (i=0; i+4 < len; i+=4) {
+        *((uint32_t*)buf) = esp_random();
+        buf += 4;
+    }
+    if (i < len) {
+        uint32_t r = esp_random();
+        while (i < len) {
+            *buf++ = r;
+            r >>= 8;
+            i++;
+        }
+    }
+
+    return 0;
+}
+
+#else
+
 #include <esp/hwrand.h>
 
 static inline int hwrand_generate_block(uint8_t *buf, size_t len) {
     hwrand_fill(buf, len);
     return 0;
 }
+
+#define MP_LOW_MEM
+
+#endif
 
 #define FREERTOS
 #define WC_NO_HARDEN
@@ -17,7 +45,6 @@ static inline int hwrand_generate_block(uint8_t *buf, size_t len) {
 
 #define NO_WOLFSSL_MEMORY
 #define NO_WOLFSSL_SMALL_STACK
-#define MP_LOW_MEM
 
 #define CUSTOM_RAND_GENERATE_BLOCK hwrand_generate_block
 
