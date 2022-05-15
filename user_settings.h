3af6120a7f35fa68baecc0f1941041cd8b683766
Maxim Kulkin <maxim.kulkin@gmail.com>
2022-05-15 16:13:13 -0400

    Fix compilation options issue for ESP-IDF

--- user_settings.h
+++ user_settings.h
@@ -1,32 +1,6 @@
 #ifndef wolfcrypt_user_settings_h
 #define wolfcrypt_user_settings_h
 
-#ifdef ESP_IDF
-
-#define WOLFSSL_ESPIDF
-
-#include <esp_system.h>
-
-static inline int hwrand_generate_block(uint8_t *buf, size_t len) {
-    int i;
-    for (i=0; i+4 < len; i+=4) {
-        *((uint32_t*)buf) = esp_random();
-        buf += 4;
-    }
-    if (i < len) {
-        uint32_t r = esp_random();
-        while (i < len) {
-            *buf++ = r;
-            r >>= 8;
-            i++;
-        }
-    }
-
-    return 0;
-}
-
-#else
-
 #include <esp/hwrand.h>
 
 static inline int hwrand_generate_block(uint8_t *buf, size_t len) {
@@ -45,8 +19,6 @@ static inline int hwrand_generate_block(uint8_t *buf, size_t len) {
 
 #define NO_WOLFSSL_MEMORY
 #define NO_WOLFSSL_SMALL_STACK
-
-#endif
 
 #define CUSTOM_RAND_GENERATE_BLOCK hwrand_generate_block
 
